cde07d063412c4d76acae200f78cde68934a82da
dipdas043 <dipdas.dip@gmail.com>
2023-04-30 21:24:41 +0600

    testing the connection

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LoginForm login = new LoginForm();
-		login.setVisible(true);
+		login.setVisible(true); //Hi this is for checking purpose.
 	}
 }
