ba18fcf0687138bc701e5f9d10cefb7e1f1a73e9
dipdas043 <dipdas.dip@gmail.com>
2023-05-03 00:20:53 +0600

    Home-2

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -5,8 +5,22 @@ import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
 import javax.swing.*;
+import gui.*;
+public class Home extends JFrame{ 
+    public void initialize(){
+        
 
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(new Color(128, 128, 255));
 
-public class Home {
+        setTitle("Welcome");
+        setSize(500, 600);
+        setMinimumSize(new Dimension(300, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    public class Home {
     
+    }
 }
