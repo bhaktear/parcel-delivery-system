9cee5456536fdb0089e4559acf2875dba131b124
Gowrab Das Dip <61190968+dipdas043@users.noreply.github.com>
2023-05-04 18:11:13 +0600

    Update Home/Welcome page

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -1,26 +1,38 @@
 package gui;
-
 import db.DBConnection;
-import java.awt.*;
-import java.awt.event.*;
-import java.sql.*;
 import javax.swing.*;
+import java.awt.event.*;
 import gui.*;
-public class Home extends JFrame{ 
-    public void initialize(){
-        
 
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBackground(new Color(128, 128, 255));
+public class Home extends JFrame{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button1 = new JButton("Log in");
+        JButton button2 = new JButton("Register");
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                LoginForm login = new LoginForm();
+                login.setVisible(true);
+                frame.dispose();
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                RegForm reg = new RegForm();
+                reg.setVisible(true);
+                frame.dispose();
+            }
+        });
 
-        setTitle("Welcome");
-        setSize(500, 600);
-        setMinimumSize(new Dimension(300, 400));
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-    public class Home {
-    
+        JLabel background = new JLabel(new ImageIcon("img/Home_baimf.jpg"));
+        background.setLayout(null);
+        background.add(button1);
+        background.add(button2);
+        button1.setBounds(450, 30, 100, 30);
+        button2.setBounds(600, 30, 100, 30);
+        background.setSize(background.getPreferredSize());
+        frame.setContentPane(background);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
     }
 }
