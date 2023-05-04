a01c7f8e83ee23e0f046ada72fd8f5f04adbc6d7
Gowrab Das Dip <61190968+dipdas043@users.noreply.github.com>
2023-05-04 18:21:32 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import gui.Home;
 
 public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Home home = new Home();
-		home.setVisible(true); //Hi this is for checking purpose.
-	}
+    public static void main(String[] args) {
+        Home home = new Home();
+    }
 }
